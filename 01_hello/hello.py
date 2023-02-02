85f27b0662039153c02da7916d894a23b9bc42d2
pampas376 <npamps03@gmail.com>
2023-02-02 18:28:08 -0500

    Upload our changes in hello01 project

---
+++ 01_hello/hello.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Author : nicolo <nicolo@localhost>
+Date   : 2023-02-03
+Purpose: Say Hello
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='hello project',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('positional',
+                        metavar='str',
+                        help='A positional argument')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    person = args.name
+    print(f'Hello, {person}!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+
+    main()
